f5dd9f98e84a8c273c517a5a4d97ffeee0d35bbc
worthant <b_dvorkin@niuitmo.ru>
2026-02-24 05:58:15 +0300

    :rocket: feat!: add WakeNet9 "HiESP" model

--- src/main.c
+++ src/main.c
@@ -1,22 +1,43 @@
-#include "driver/i2s_common.h"
-#include "driver/i2s_std.h"
-#include "driver/i2s_types.h"
-#include "esp_err.h"
 #include "esp_log.h"
-#include "esp_system.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "hal/i2s_types.h"
-#include "i2s_mic.h"
-#include "portmacro.h"
-#include <math.h>
-#include <stdio.h>
 #include "esp_wn_iface.h"
+#include "esp_wn_models.h"
+#include "i2s_mic.h"
+#include "model_path.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 static const char *TAG = "main";
 
 void app_main() {
     ESP_LOGI(TAG, "--- entering app main ---");
+
+    srmodel_list_t *models = esp_srmodel_init("model");
+    if (models == NULL) {
+        ESP_LOGE(TAG,
+                 "Failed to initialize model list. Check partition table!");
+        return;
+    }
+
+    char *wn_name = esp_srmodel_filter(models, ESP_WN_PREFIX, "hiesp");
+    if (wn_name == NULL) {
+        ESP_LOGE(TAG, "Model 'hiesp' not found in Flash. Check menuconfig!");
+        return;
+    }
+    ESP_LOGI(TAG, "--- found model: %s ---", wn_name);
+
+    esp_wn_iface_t *wakenet = esp_wn_handle_from_name(wn_name);
+
+    model_iface_data_t *model_data = wakenet->create(wn_name, DET_MODE_90);
+    if (model_data == NULL) {
+        ESP_LOGE(TAG, "Failed to create model instance. Not enough PSRAM?");
+        return;
+    }
+
+    ESP_LOGI(TAG, "--- WakeNet init DONE ---");
+
+    int chunk_size = wakenet->get_samp_chunksize(model_data);
+    ESP_LOGI(TAG, "Model expects chunks of %d samples (int16_t)", chunk_size);
+
     ESP_ERROR_CHECK(i2s_mic_init());
-    i2s_mic_demo();
+    //i2s_mic_demo();
 }
