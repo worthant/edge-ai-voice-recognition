7d1e0ce926c7430e3c602dbb644468c3bd9356b2
worthant <b_dvorkin@niuitmo.ru>
2026-04-27 22:56:28 +0300

    :hammer: feat!: Add converted ds-cnn m, tflite-micro&esp-nn

---
+++ src/model_data.h
@@ -0,0 +1,9 @@
+// Автоматически сгенерировано export_to_c.py
+// Источник: ds_cnn_qat_int8.tflite
+// Сгенерировано: 2026-04-27 14:31:55 UTC
+// Размер: 267592 байт
+#pragma once
+#include <stdint.h>
+
+extern const unsigned int g_model_data_size;
+extern const unsigned char g_model_data[];
